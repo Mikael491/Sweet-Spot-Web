cb8eb024c6c08a644241a4eec12f83b602204e87
Marshall Agharanya <marshall.agharanya@gmail.com>
2016-07-11 15:45:37 -0700

    create show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destory]
   def index
     @products = Product.all
   end
@@ -9,9 +10,19 @@ class ProductsController < ApplicationController
   def new
   end
   def edit
+    # @product = Product(params[id])
   end
   def update
   end
   def destroy
   end
+
+  private 
+  def find_product
+    @product = Product.find(params[:id])
+  end
+  def product_params
+
+  end
+
 end
