622cbb138cdf3f8e4ae3be5a97be6b461e380805
Marshall Agharanya <marshall.agharanya@gmail.com>
2016-07-13 16:16:41 -0700

    forms include quantity field, update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711234405) do
+ActiveRecord::Schema.define(version: 20160713231035) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160711234405) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "quantity"
   end
 
 end
