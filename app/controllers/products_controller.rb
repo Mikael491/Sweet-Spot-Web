15d121787302a5377e8679147bf2b3f06130134f
Mikael <miketekle14@gmail.com>
2016-07-14 11:43:44 -0700

    create checkbox fields in products form for all categories available

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -57,7 +57,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :image)
+    params.require(:product).permit(:name, :description, :price, :image, category_ids:[])
   end
 
 end
