3f6905dfb559fbf8b5434d1ae442016e20f7dc3a
Mikael <miketekle14@gmail.com>
2016-07-13 21:48:19 -0700

    Complete working join table between products and categories with has many through associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-	
+	has_many :categories_products
+	has_many :products, through: :categories_products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713233614) do
+ActiveRecord::Schema.define(version: 20160714044041) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+  end
+
+  create_table "categories_products", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "products", force: :cascade do |t|
